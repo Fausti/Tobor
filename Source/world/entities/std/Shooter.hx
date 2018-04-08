5f66ba02051abcb7cd246f9ef371dc1eb27f5af3
Matthias Faust <faust.matthias@googlemail.com>
2018-04-08 10:55:56 +0200

    Nachladebild für Geschütz

--- Source/world/entities/std/Shooter.hx
+++ Source/world/entities/std/Shooter.hx
@@ -8,14 +8,17 @@ import world.entities.interfaces.IElectric;
  */
 class Shooter extends EntityDynamic implements IElectric {
 	var SPR_SHOOTER:Array<Sprite>;
+	var SPR_RELOAD:Array<Sprite>;
 	var reloadTime:Float = 0.0;
 	
 	public function new() {
 		super();
 		
 		SPR_SHOOTER = [];
+		SPR_RELOAD = [];
 		for (i in 0 ... 4) {
 			SPR_SHOOTER.push(Gfx.getSprite(80 + i * 16, 336));
+			SPR_RELOAD.push(Gfx.getSprite(144 + i * 16, 336));
 		}
 		
 		z = Room.LAYER_OVERLAY;
@@ -30,7 +33,11 @@ class Shooter extends EntityDynamic implements IElectric {
 	}
 	
 	override public function render() {
-		Gfx.drawSprite(x * Tobor.TILE_WIDTH, y * Tobor.TILE_HEIGHT, SPR_SHOOTER[type]);
+		if (reloadTime == 0.0) {
+			Gfx.drawSprite(x * Tobor.TILE_WIDTH, y * Tobor.TILE_HEIGHT, SPR_SHOOTER[type]);
+		} else {
+			Gfx.drawSprite(x * Tobor.TILE_WIDTH, y * Tobor.TILE_HEIGHT, SPR_RELOAD[type]);
+		}
 	}
 	
 	override public function switchStatus() {
